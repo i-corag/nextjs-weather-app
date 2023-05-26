8b5b0e54069c474421106ad74d50e514ecb78dcb
i-corag <icoraglio@gmail.com>
2023-05-26 04:21:06 +0200

    first commit

---
+++ components/Loading.jsx
@@ -0,0 +1,11 @@
+import { PuffLoader } from "react-spinners"
+
+const Loading = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <PuffLoader color='#e89e9e' />
+        </div>
+    )
+}
+
+export default Loading
